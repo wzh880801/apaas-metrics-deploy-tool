4e548beecbcbcf38f2e324711f6af689b5e1feed
王重 <wangzhong.1@bytedance.com>
2025-01-08 16:37:13 +0800

    mkdir prom dirs

--- start.sh
+++ start.sh
@@ -12,6 +12,18 @@ if ! command -v docker-compose &> /dev/null; then
     exit 1
 fi
 
+if ! [ -d "./prometheus/data" ]; then
+    mkdir ./prometheus/data && chmod -R 777 ./prometheus/data
+else
+    chmod -R 777 ./prometheus/data
+fi
+
+if ! [ -d "./prometheus/logs" ]; then
+    mkdir ./prometheus/logs && chmod -R 777 ./prometheus/logs
+else
+    chmod -R 777 ./prometheus/logs
+fi
+
 # 判断是否是首次启动
 # 区别在于首次启动会将 grafana 的默认配置挂载到 grafana，这样有默认的看板
 if [ -f "./grafana/data/grafana.db" ]; then
